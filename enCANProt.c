c5b937d3434e3f848f238ce883f41e59343d74dc
Oleg <o.pocheptsov@gmail.com>
2020-04-02 11:13:27 +0300

    enCANProt was refactored;

--- enCANProt.c
+++ enCANProt.c
@@ -8,288 +8,194 @@
 #endif
 
 #ifndef ENP_FREERTOS
-// Положение выходных данных в буфере передачи
-static int SMP_THead[SMP_CHANNUM] = {0, 0}; // голова
-static int SMP_TTail[SMP_CHANNUM] = {0, 0}; // хвост
+// Ring buffer pointers
+static int txHead = 0;
+static int txTail = 0;
+// transmit buffer
+static enCANMessage_t *txBuff = NULL;
 
-static smpMsg_t *SMP_TBuff[SMP_CHANNUM]; // передачи
-
-static uint16_t SMP_TMask = 0;
+static uint16_t txMask = 0;
 #endif
 
-// Буфер сообщений
-static smpMsg_t *SMP_RBuff; // приёма
+// recieve buffer
+static enCANMessage_t *rxBuff = NULL;
+static uint16_t rxMask = 0;
+static int volatile rxHead = 0;
+static int rxTail = 0;
+char ENP_RBuf[2][ENCAN_RBUF_SIZE];
+static uint16_t ENP_RHead[2] = {0, 0};
+static uint16_t ENP_RTail[2] = {0, 0};
 
-// Маски буферов
-static uint16_t SMP_RMask = 0;
+// ENP handler
+static void ENP_MsgHandler(enCANMessage_t *msg);
 
-// Положение входных данных в буфере приёма
-static int volatile SMP_RHead = 0; // голова
-static int SMP_RTail = 0;          // хвост
+// List of handlers
+static enCANMsg_tHANDLER MsgHandler[ENCAN_MSGHANDLERSNUM] = {ENP_MsgHandler};
 
-// Буфер приёма протокола ENP
-char ENP_RBuf[2][ENP_RBUF_SIZE];
-
-// Положение входных данных в буфере приёма
-static uint16_t ENP_RHead[2] = {0, 0}; // голова
-static uint16_t ENP_RTail[2] = {0, 0}; // хвост
-
-// Обработчик сообщений протокола ENP
-static void ENP_MsgHandler(smpMsg_t *msg);
-
-// Список обработчиков сообщений
-static smpMsg_tHANDLER MsgHandler[SMP_MSGHANDLERSNUM] = {ENP_MsgHandler};
-
-// Кол-во обработчиков сообщений
+// number of handlers
 static uint16_t MsgHandlerNum = 1;
 
-// Флаги ошибок портокола
-uint32_t SMP_Error = 0;
+uint32_t ENCAN_Error = 0;
 
-// Функция получения времени
 static uint32_t TimeDummy(void) { return 0; }
-uint32_t (*SMP_Time)(void) = TimeDummy;
+uint32_t (*ENCAN_Time)(void) = TimeDummy;
 
-// Функция передачи сообщения
-static uint8_t TxDummy(smpMsg_t *msg) { return 0; }
-uint8_t (*SMP_Tx)(smpMsg_t *msg) = TxDummy;
+static uint8_t TxDummy(enCANMessage_t *msg) { return 0; }
+uint8_t (*ENCAN_Tx)(enCANMessage_t *msg) = TxDummy;
 
-// Функция приема сообщения
-uint8_t SMP_Rx(smpMsg_t *msg) {
+// get message
+uint8_t ENCAN_Rx(enCANMessage_t *msg) {
   int i;
 
   if (msg) {
-    // определяем позицию нового символа в буфере
-    i = (SMP_RHead + 1) & SMP_RMask;
-    // переписываем данные в буфер
-    if (i != SMP_RTail) {
-      SMP_RHead = i;
-      SMP_RBuff[i] = *msg;
+    i = (rxHead + 1) & rxMask;
+    if (i != rxTail) {
+      rxHead = i;
+      rxBuff[i] = *msg;
       return 1;
     }
-    // ошибка переполнения буфера
-    SMP_Error |= SMP_ERROR_RXOVERFLOW;
+    ENCAN_Error |= ENCAN_ERROR_RXOVERFLOW;
   }
   return 0;
 }
 
 #ifdef ENP_FREERTOS
-/*Инициализация портокола SMP*/
-void SMP_Init(
-    // буфер приема (1 << rmask)
-    smpMsg_t *rbuff, uint16_t rmask,
-    // функция передачи сообщения
-    uint8_t (*txfunc)(smpMsg_t *msg),
-    // функция получения времени
-    uint32_t (*timefunc)(void)) {
-
-  SMP_RBuff = rbuff;
-  SMP_RMask = rmask;
+void ENCAN_Init(enCANMessage_t *rbuff, uint16_t rmask,
+                uint8_t (*txfunc)(enCANMessage_t *msg),
+                uint32_t (*timefunc)(void)) {
+  rxBuff = rbuff;
+  rxMask = rmask;
 
   if (txfunc) {
-    SMP_Tx = txfunc;
+    ENCAN_Tx = txfunc;
   }
   if (timefunc) {
-    SMP_Time = timefunc;
+    ENCAN_Time = timefunc;
   }
 }
 #else
-// Инициализация портокола SMP
-void SMP_Init(
-    // буфер приема (1 << rmask)
-    smpMsg_t *rbuff, uint16_t rmask,
-    // буфера передачи (1 << tmask)
-    smpMsg_t *tbuff1, smpMsg_t *tbuff2, uint16_t tmask,
-    // функция передачи сообщения
-    uint8_t (*txfunc)(smpMsg_t *msg),
-    // функция получения времени
-    uint32_t (*timefunc)(void)) {
 
-  SMP_RBuff = rbuff;
-  SMP_TBuff[SMP_CHAN1] = tbuff1;
-  SMP_TBuff[SMP_CHAN2] = tbuff2;
-  SMP_RMask = rmask;
-  SMP_TMask = tmask;
+void ENCAN_Init(enCANMessage_t *rbuff, uint16_t rmask, enCANMessage_t *tbuff,
+                uint16_t tmask, uint8_t (*txfunc)(enCANMessage_t *msg),
+                uint32_t (*timefunc)(void)) {
+  rxBuff = rbuff;
+  txBuff = tbuff;
+  rxMask = rmask;
+  txMask = tmask;
 
   if (txfunc) {
-    SMP_Tx = txfunc;
+    ENCAN_Tx = txfunc;
   }
   if (timefunc) {
-    SMP_Time = timefunc;
+    ENCAN_Time = timefunc;
   }
-}
-// Передача сообщения в очередь передачи
-static uint8_t SMP_PutMessage(smpMsg_t msg) {
-  const int _ch = msg.Attr.chan;
-
-  if (SMP_TBuff[_ch]) {
-    // определяем позицию нового символа в буфере передачи
-    int i = (SMP_THead[_ch] + 1) & SMP_TMask;
-    // если буфер переполнен, выставляем ошибку
-    if (i != SMP_TTail[_ch]) {
-      // добавляем сообщение в буфер передачи
-      SMP_TBuff[_ch][i] = msg;
-      SMP_THead[_ch] = i;
-    } else
-      SMP_Error |= SMP_ERROR_TXOVERFLOW;
-  }
-
-  return 0;
 }
 #endif
 
-// Добавление обработчика сообщений
-uint8_t SMP_AddHandler(smpMsg_tHANDLER func) {
-  int i;
-
-  if (func && MsgHandlerNum < SMP_MSGHANDLERSNUM) {
-    // проверяем был ли добавлен обработчик ранее
-    for (i = 0; i < MsgHandlerNum && MsgHandler[i] != func; i++) {
-    }
-    // добавляем обработчик
-    if (i == MsgHandlerNum) {
-      MsgHandler[MsgHandlerNum++] = func;
-    }
-    return 1;
-  } else
-    return 0;
-}
-
-// Обработка очереди сообщений
-void SMP_Proc() {
-  // обработка очереди полученых сообщений
-  while (SMP_RHead != SMP_RTail) {
-    SMP_RTail = (SMP_RTail + 1) & SMP_RMask;
+void ENCAN_Proc() {
+  while (rxHead != rxTail) {
+    rxTail = (rxTail + 1) & rxMask;
     for (int i = 0; i < MsgHandlerNum; i++)
-      MsgHandler[i](SMP_RBuff + SMP_RTail);
+      MsgHandler[i](rxBuff + rxTail);
   }
 
 #ifndef ENP_FREERTOS
-  // отправка сообщений из очереди
-  for (int ch = SMP_CHAN1; ch < SMP_CHANNUM; ch++) {
-    if (SMP_TBuff[ch]) {
-      while (SMP_TTail[ch] != SMP_THead[ch]) {
-        int i = (SMP_TTail[ch] + 1) & SMP_TMask;
-        if (SMP_Tx(SMP_TBuff[ch] + i)) {
-          SMP_TTail[ch] = i;
-        } else {
-          break;
-        }
+  if (txBuff) {
+    while (txTail != txHead) {
+      int i = (txTail + 1) & txMask;
+      if (ENCAN_Tx(txBuff + i)) {
+        txTail = i;
+      } else {
+        break;
       }
     }
   }
 #endif
 }
 
-/* Формирование и добавление в очередь сообщения
-    msgid - идентификатор сообщения
-    *data - указатель на данные сообщения
-    size - размер данных сообщения <= 8 */
-uint8_t SMP_AddBroadcast(int msgid, const void * const data, int size) {
-
-  int _res = 0;
-  smpMsg_t _msg;
+static void AddBroadcast(int msgid, const void *const data, int size) {
+  enCANMessage_t _msg;
 #ifdef ENP_FREERTOS
   if (size <= 8 && data) {
-    // формируем сообщение
     memcpy(&_msg.data, data, size);
     _msg.Attr.id = msgid;
     _msg.Attr.size = size;
-    _msg.Attr.chan = SMP_CHAN1;
-    SMP_Tx(&_msg);
+    ENCAN_Tx(&_msg);
   }
 #else
   if (size <= 8 && data) {
-    // формируем сообщение
     memcpy(&_msg.data, data, size);
     _msg.Attr.id = msgid;
     _msg.Attr.size = size;
-    // отправляем сообщение
-    _msg.Attr.chan = SMP_CHAN1;
-    if (SMP_PutMessage(_msg))
-      _res = 1;
-    _msg.Attr.chan = SMP_CHAN2;
-    if (SMP_PutMessage(_msg))
-      _res = 1;
+    if (txBuff) {
+      txHead = (txHead + 1) & txMask;
+      if (txHead != txTail) {
+        txBuff[txHead] = _msg;
+      } else
+        ENCAN_Error |= ENCAN_ERROR_TXOVERFLOW;
+    }
   }
 #endif
-
-  return _res;
 }
 
-/*  Приём символа для протокола ENP
- - возвращает -1 если буфер приёма пуст, иначе принятый символ */
-static int SMP_getc(int i) {
+static int Getc(int i) {
   if (ENP_RTail[i] != ENP_RHead[i]) {
-    return ENP_RBuf[i][ENP_RTail[i] = (ENP_RTail[i] + 1) & ENP_RBUF_MASK];
+    return ENP_RBuf[i][ENP_RTail[i] = (ENP_RTail[i] + 1) & ENCAN_RBUF_MASK];
   } else {
     return -1;
   }
 }
-int SMP_getc1() { return SMP_getc(0); }
-int SMP_getc2() { return SMP_getc(1); }
+int ENCAN_getc1() { return Getc(0); }
+int ENCAN_getc2() { return Getc(1); }
 
-/*  Передача строки из n символов для протокола ENP учетом места для ответа
-     - возвращает 1 если строка помещена в буфер передачи, иначе 0 */
-static int SMP_putn(const char *s, int n, int i) {
-  static uint16_t pcntr[2] = {0, 0};
-  static uint16_t id[2] = {SMP_SEND1, SMP_SEND2};
+int ENCAN_putn(const char *s, int n) {
+  static uint16_t pcntr = 0;
 
   for (; n > 0; n -= 8) {
     if (n >= 8) {
-      SMP_AddBroadcast(id[i] + pcntr[i], (void *)s, 8);
+      AddBroadcast(ENCAN_SEND1 + pcntr, (void *)s, 8);
     } else {
-      SMP_AddBroadcast(id[i] + pcntr[i], (void *)s, n);
+      AddBroadcast(ENCAN_SEND1 + pcntr, (void *)s, n);
     }
-    // меняем счетчик пакетов
-    pcntr[i] = (pcntr[i] + 1) & SMP_MASK;
+    pcntr = (pcntr + 1) & ENCAN_MASK;
     s += 8;
   }
   return 1;
 }
-int SMP_putn1(const char *s, int n) { return SMP_putn(s, n, 0); }
-int SMP_putn2(const char *s, int n) { return SMP_putn(s, n, 1); }
 
-// Обработчик сообщений протокола ENP
-static void ENP_MsgHandler(smpMsg_t *msg) {
+static void ENP_MsgHandler(enCANMessage_t *msg) {
   unsigned char *c = (void *)&msg->data;
   int i, j, n, msgid;
   static uint32_t ptime[2] = {0, 0};
   static uint16_t pcntr[2] = {0, 0};
-  uint32_t time = SMP_Time();
+  uint32_t time = ENCAN_Time();
   const uint32_t _id = msg->Attr.id;
 
-  // определяем поток ENP
-  if (_id >= SMP_RECV1 && _id <= SMP_RECV1 + SMP_MASK) {
+  if (_id >= ENCAN_RECV1 && _id <= ENCAN_RECV1 + ENCAN_MASK) {
     i = 0;
-    msgid = SMP_RECV1;
-  } else if (_id >= SMP_RECV2 && _id <= SMP_RECV2 + SMP_MASK) {
+    msgid = ENCAN_RECV1;
+  } else if (_id >= ENCAN_RECV2 && _id <= ENCAN_RECV2 + ENCAN_MASK) {
     i = 1;
-    msgid = SMP_RECV2;
+    msgid = ENCAN_RECV2;
   } else {
     return;
   }
 
-  // сбрасываем счетчик пакетов
   if (ptime[i] < time) {
     pcntr[i] = _id - msgid;
   }
 
-  // проверяем на совпадение счетчика пакетов
   if (_id == msgid + pcntr[i]) {
     for (n = 0; n < msg->Attr.size; n++) {
-      // определяем позицию нового символа в буфере передачи
-      j = (ENP_RHead[i] + 1) & ENP_RBUF_MASK;
-      // добавляем символ в буфер передачи, если там есть место
+      j = (ENP_RHead[i] + 1) & ENCAN_RBUF_MASK;
       if (j != ENP_RTail[i]) {
         ENP_RBuf[i][ENP_RHead[i] = j] = *c++;
       } else {
-        SMP_Error |= SMP_ERROR_RXOVERFLOW;
+        ENCAN_Error |= ENCAN_ERROR_RXOVERFLOW;
         break;
       }
     }
-    // меняем счетчик пакетов
-    pcntr[i] = (pcntr[i] + 1) & SMP_MASK;
-    ptime[i] = time + SMP_ENP_TIMEOUT;
+    pcntr[i] = (pcntr[i] + 1) & ENCAN_MASK;
+    ptime[i] = time + ENCAN_ENP_TIMEOUT;
   }
 }
--- enCANProt.h
+++ enCANProt.h
@@ -1,93 +1,76 @@
 /****************************************************************************
  *                      Embedded node protocol(ENP)                         *
  ****************************************************************************/
-#ifndef SMP_H_
-#define SMP_H_
+#ifndef EN_CAN_H_
+#define EN_CAN_H_
 #include "enpHelper.h"
-#include "stdint.h"
 
-extern uint32_t SMP_Error;
+extern uint32_t ENCAN_Error;
 
-// Флаги ошибок портокола
-#define SMP_ERROR_NOERROR 0x00000000 // нет ошибок
-#define SMP_ERROR_RXOVERFLOW 0x10000000 // переполнение приемного буфера
-#define SMP_ERROR_TXOVERFLOW 0x20000000 // переполнение передающего буфера
+// Protocol errors
+#define ENCAN_ERROR_NOERROR 0x00000000
+#define ENCAN_ERROR_RXOVERFLOW 0x10000000  // overflow rx buffer
+#define ENCAN_ERROR_TXOVERFLOW 0x20000000  // overflow tx buffer
 
-// Cообщения связи с пунктом контроля
-#define SMP_RECV1 1808 // данные принятые с канала 1
-#define SMP_SEND1 1824 // данные для передачи по каналу 1
-#define SMP_RECV2 1840 // данные принятые с канала 2
-#define SMP_SEND2 1856 // данные для передачи по каналу 2
+// CAN ID
+#define ENCAN_RECV1 1808
+#define ENCAN_SEND1 1824
+#define ENCAN_RECV2 1840
+#define ENCAN_SEND2 1856
 
-// Маска счетчика пакетов канала передачи данных
-#define SMP_MASK 0x0F
-// Таймаут сообщений (мс)
-#define SMP_ENP_TIMEOUT 1000
+// Mask for counter
+#define ENCAN_MASK 0x0F
+// Timeout for can messages (ms)
+#define ENCAN_ENP_TIMEOUT 1000
 
-// Идентификаторы каналов
-enum { SMP_CHAN1, SMP_CHAN2, SMP_CHANNUM };
-
-// Сообщение
+// message
 typedef struct {
-  uint8_t data[8]; // данные
-  // аттрибуты
+  uint8_t data[8];  // data buff for CAN classic
   struct {
-    unsigned id : 11;  // идентификатор
-    unsigned size : 4; // размер
-    unsigned chan : 1; // канал
+    unsigned id : 11;   // standart id
+    unsigned size : 4; 
   } Attr;
-} smpMsg_t;
+} enCANMessage_t;
 
-// Размер буфера приема протокола ENP
-#define ENP_RBUF_SIZE 128
+#define ENCAN_RBUF_SIZE 128
 
-// Маски индексов
-#define ENP_RBUF_MASK (ENP_RBUF_SIZE - 1)
-#if (ENP_RBUF_SIZE & ENP_RBUF_MASK)
-#error ENP_RBUF_SIZE is not a power of 2
+// Index mask
+#define ENCAN_RBUF_MASK (ENCAN_RBUF_SIZE - 1)
+#if (ENCAN_RBUF_SIZE & ENCAN_RBUF_MASK)
+#error ENCAN_RBUF_SIZE is not a power of 2
 #endif
 
-// Обработчик сообщения протокола
-typedef void (*smpMsg_tHANDLER)(smpMsg_t *msg);
+// protocol handler function
+typedef void (*enCANMsg_tHANDLER)(enCANMessage_t* msg);
 
-// Максимальное кол-во обработчиков сообщений
-#define SMP_MSGHANDLERSNUM 20
+// Max count of protocol handlers
+#define ENCAN_MSGHANDLERSNUM 20
 
-// Инициализация портокола
+// Init
 #ifdef ENP_FREERTOS
-void SMP_Init(smpMsg_t *, uint16_t,
-                     uint8_t (*)(smpMsg_t *msg), uint32_t (*)(void));
+void ENCAN_Init(enCANMessage_t*,
+              uint16_t,
+              uint8_t (*)(enCANMessage_t* msg),
+              uint32_t (*)(void));
 #else
-void SMP_Init(smpMsg_t *, uint16_t, smpMsg_t *, smpMsg_t *, uint16_t,
-                     uint8_t (*)(smpMsg_t *msg), uint32_t (*)(void));
+void ENCAN_Init(enCANMessage_t*,
+              uint16_t,
+              enCANMessage_t*,
+              uint16_t,
+              uint8_t (*)(enCANMessage_t* msg),
+              uint32_t (*)(void));
 #endif
 
+void ENCAN_Proc(void);
+void ENCAN_TxProc(void);
 
-// Процедура протокола
-void SMP_Proc(void);
-void SMP_TxProc(void);
+// Get time function
+extern uint32_t (*ENCAN_Time)(void);
 
-// Функция получения времени
-extern uint32_t (*SMP_Time)(void);
+// Send CAN message function
+extern uint8_t (*ENCAN_Tx)(enCANMessage_t* msg);
 
-// Функция передачи сообщения
-extern uint8_t (*SMP_Tx)(smpMsg_t *msg);
-
-// Функция приема сообщения
-uint8_t SMP_Rx(smpMsg_t *msg);
-
-// Добавление обработчика сообщений
-uint8_t SMP_AddHandler(smpMsg_tHANDLER func);
-
-// Формирование и добавление в очередь широковещательного сообщения
-uint8_t SMP_AddBroadcast(int, const void * const, int);
-
-// Приём символа для протокола ENP
-int SMP_getc1(void);
-int SMP_getc2(void);
-
-// Передача строки из n символов для протокола ENP
-int SMP_putn1(const char *s, int n);
-int SMP_putn2(const char *s, int n);
+// Rx message function
+uint8_t ENCAN_Rx(enCANMessage_t* msg);
 
 #endif
